b0715a52cf4821bf2b33af2f3d4356f4f200c49e
AlexPuenteJ <problemscompeititve1551@gmail.com>
2023-11-29 22:39:04 -0500

    fdsfds

---
+++ src/PrimeraApp.js
@@ -0,0 +1,16 @@
+//funtional components
+import React from 'react'//irm + tap para ser fácil
+const PrimeraApp=()=>{
+    const saludo='Hola Mundo';
+    return (
+        <>   
+        <h1>{saludo}</h1>
+             {/*<pre>{JSON.stringify(saludo,null,3)}</pre>*/}
+             <p>hi hi ji</p>
+        </>
+       
+
+    );
+}
+
+export default PrimeraApp;
